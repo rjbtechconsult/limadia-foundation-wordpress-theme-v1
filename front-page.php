db6917b16b07f0f9a51b31377cb97caa703c778e
Justice Markwei <rjbtechconsult@gmail.com>
2025-02-22 21:27:49 +0000

    template name

--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * The template for displaying all pages
- *
+ * TemplateName: Home
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
  * and that other 'pages' on your WordPress site may use a
